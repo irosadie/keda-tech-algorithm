c63da75bf78a6a3ebb1802a17ea01659af8efcf3
bangimron <imron.rosadi@jatis.com>
2023-08-08 07:37:56 +0700

    fix typo

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,4 @@
 module.exports = {
-  testEnvironment: "node", // Menentukan lingkungan pengujian (node untuk pengujian di lingkungan Node.js)
-  testMatch: ["**/*.test.js"], // Pola untuk mencocokkan file pengujian (contoh: foo.test.js, bar.test.js)
-  // Tambahan opsi-opsi kustom lainnya bisa Anda tambahkan di sini
+  testEnvironment: "node",
+  testMatch: ["**/*.test.js"],
 };
